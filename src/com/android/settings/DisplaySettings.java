a72b0b4c1010c5c98b7da18cbfd318701257602e
Jonas Salo <salomannen1234@gmail.com>
2026-06-23 12:55:11 +0200

    Settings: add selectable touch polling rates

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -30,6 +30,7 @@ import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.DisplayScreen;
 import com.android.settings.display.ExternalDisplayPreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
+import com.android.settings.display.HighTouchPollingRatePreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.EnableBlursPreferenceController;
@@ -106,6 +107,8 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new EmulateDisplayCutoutPreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new HighTouchPollingRatePreferenceController(
+                context, KEY_HIGH_TOUCH_POLLING_RATE));
 
         if (DesktopSettingsUtils.shouldShowTopLevelDeviceCategory(context)) {
             controllers.add(new ExternalDisplayPreferenceController(context, lifecycle));
